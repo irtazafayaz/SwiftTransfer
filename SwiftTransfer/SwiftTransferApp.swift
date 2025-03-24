1b14fe9b0fae963cafd1d66548d21643be16a99c
irtazafayaz <irtazab135@gmail.com>
2025-03-25 01:59:22 +0500

    Initial Commit

---
+++ SwiftTransfer/SwiftTransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTransferApp.swift
+//  SwiftTransfer
+//
+//  Created by Irtaza Fiaz on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
